deb977dcc2e719f6c4520bb44719d6811fa69aad
defifofum <defifofum0@gmail.com>
2021-06-16 16:50:27 -0400

    feat: Add function to interface

--- contracts/interfaces/IApeFactory.sol
+++ contracts/interfaces/IApeFactory.sol
@@ -21,6 +21,7 @@ interface IApeFactory {
     function allPairsLength() external view returns (uint);
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
+    function createPairDefault(address tokenA, address tokenB) external returns (address pair);
 
     function setFeeTo(address) external;
     function setFeeToSetter(address) external;
